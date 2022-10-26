c8068de5019a7486bbfef048bfd69d2a8295f558
Walter <you@example.com>
2022-10-26 17:05:15 -0300

    atualizando

---
+++ operadores_matematicos_logicos.php
@@ -0,0 +1,12 @@
+<?php 
+
+$a = 2;
+$b = 2;
+
+if ($a == $b OR $a == 1) {
+    echo 'No IF';
+}else{
+    echo 'No ELSE';
+}
+
+
---
+++ operadores_matematicos_relacionais.php
@@ -0,0 +1,10 @@
+<?php 
+
+$a = 2;
+$b = 3; 
+
+if ($a <> $b) {
+    echo "é igual";
+}else{
+    echo "não é igual";
+}
